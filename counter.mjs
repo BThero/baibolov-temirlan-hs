7a83399528455c3060a02c8a2be9c9fae1397376
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2022-06-15 17:08:14 +0200

    try ecmascript

---
+++ counter.mjs
@@ -0,0 +1,6 @@
+var ctr = 0;
+export { ctr };
+
+export const counter = function () {
+	return ++ctr;
+};
---
+++ other.mjs
@@ -0,0 +1,10 @@
+import { counter, ctr as value } from './counter.mjs';
+
+console.log(counter());
+console.log(counter());
+
+console.log('value', value);
+
+console.log(counter());
+
+console.log('value', value);
